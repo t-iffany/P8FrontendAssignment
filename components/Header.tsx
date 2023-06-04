1a7670010c97be2d3a5952ef60ed20968b1decf9
t-iffany <tiffanyxleong@gmail.com>
2023-06-04 14:49:59 -0700

    Add styling to Header component. Add alignment, padding, box-shadow.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <div>
+    <div className={styles.header}>
       <Image src="/p8Wordmark_sm.svg" alt="logo" width={120} height={40} />
-      <MenuRoundedIcon />
+      <MenuRoundedIcon fontSize="large" />
     </div>
   );
 };
