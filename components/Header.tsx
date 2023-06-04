f230ad3cc8df1537f40b76baf961cfba9c8efa00
t-iffany <tiffanyxleong@gmail.com>
2023-06-04 13:38:48 -0700

    Add logo image to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const Header: React.FC = () => {
   return (
     <div>
-      Header
+      <Image src="/p8Wordmark_sm.svg" alt="logo" width={120} height={40} />
     </div>
   );
 };
