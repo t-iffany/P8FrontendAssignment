95c93e92d9c3d90c9e9b5b02d3f507ed651dee24
t-iffany <tiffanyxleong@gmail.com>
2023-06-04 14:05:57 -0700

    Add MenuRoundedIcon into Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
 const Header: React.FC = () => {
   return (
     <div>
       <Image src="/p8Wordmark_sm.svg" alt="logo" width={120} height={40} />
+      <MenuRoundedIcon />
     </div>
   );
 };
