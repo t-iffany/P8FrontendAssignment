7cc8b467c2fb00dfed5368df7473c47b901828e6
t-iffany <tiffanyxleong@gmail.com>
2023-06-04 23:26:12 -0700

    Add and style PaymentCard component

---
+++ components/PaymentCard.tsx
@@ -0,0 +1,31 @@
+import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styles from "./PaymentCard.module.css";
+
+const PaymentCard: React.FC = () => {
+  return (
+    <Card className={styles.card}>
+      <CardContent className={styles.cardContent}>
+        <Typography className={styles.title}>
+          Your total monthly payment will be
+        </Typography>
+        <Typography className={styles.payment}>
+          853
+        </Typography>
+        <Typography className={styles.subText}>
+          /month
+        </Typography>
+      </CardContent>
+      {/* <CardActions> */}
+      {/* <div className={styles.buttonContainer}> */}
+      <Button variant="contained" className={styles.button}>Apply Today</Button>
+      {/* </div> */}
+      {/* </CardActions> */}
+    </Card>
+  );
+};
+
+export default PaymentCard;
