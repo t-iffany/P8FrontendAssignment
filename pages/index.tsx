9a9911c117972273b3b944f97351dc07613365c3
t-iffany <tiffanyxleong@gmail.com>
2023-06-04 23:33:02 -0700

    Import Main component into index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import Header from '../components/Header';
+import Main from '../components/Main';
 
 const Home: NextPage = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Main />
+    </>
   );
 };
 
