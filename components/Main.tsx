c4b35873159610b676150dc2f6f98541baca768f
t-iffany <tiffanyxleong@gmail.com>
2023-06-06 12:32:51 -0700

    Add useState for mortgageCalculation parameters. Implement handleTermOfLoanChange.

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,27 @@
 import styles from "./Main.module.css";
 import LeftSection from "./LeftSection";
-import PaymentCard from "./PaymentCard";
+import SliderComponent from "./SliderComponent";
+import TermYears from "./TermYears";
+// import PaymentCard from "./PaymentCard";
+import { useState } from "react";
 
 const Main = () => {
+
+  // const [state, setState] = useState({
+  //   principal: 250000,
+  //   annualInterestRate: 1.5,
+  //   termOfLoan: 25,
+  // });
+
+  const [principal, setPrincipal] = useState<number>(250000);
+  const [annualInterestRate, setAnnualInterestRate] = useState<number>(1.5);
+  const [termOfLoan, setTermOfLoan] = useState<number>(25);
+
+  const handleTermOfLoanChange = (currentTermOfLoan: number) => {
+    setTermOfLoan(currentTermOfLoan);
+  };
+
+
   return (
     <div className={styles.main}>
 
@@ -11,12 +30,18 @@ const Main = () => {
 
       <div className={styles.container}>
 
-        <div className={styles.left}>
+        {/* <div className={styles.left}>
           <LeftSection />
+        </div> */}
+        <div>
+          <SliderComponent />
+          <SliderComponent />
+          <TermYears termOfLoan={termOfLoan} setTermOfLoan={handleTermOfLoanChange} />
+          <p>Selected Term Of Loan: {termOfLoan} Years</p>
         </div>
 
         <div className={styles.right}>
-          <PaymentCard />
+          {/* <PaymentCard /> */}
         </div>
 
       </div>
